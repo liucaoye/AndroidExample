e8a7f414d753a695cdadc578cf9274be1ebd4077
刘彦 <liuyan@yinker.com>
2016-01-18 19:38:06 +0800

    歌詞を追加します

---
+++ ExampleApp/app/src/main/java/com/example/app/lyrics/mode/LyricObject.java
@@ -0,0 +1,13 @@
+package com.example.app.lyrics.mode;
+
+/**
+ * @author LIUYAN
+ * @date 2016/1/18 0018
+ * @time 18:39
+ */
+public class LyricObject {
+    public int beginTime;
+    public int endTime;
+    public int duration;
+    public String lrc;
+}
---
+++ ExampleApp/app/src/main/java/com/example/app/lyrics/view/LyricView.java
@@ -0,0 +1,24 @@
+package com.example.app.lyrics.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * @author LIUYAN
+ * @date 2016/1/18 0018
+ * @time 18:50
+ */
+public class LyricView extends View {
+    public LyricView(Context context) {
+        super(context);
+    }
+
+    public LyricView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public LyricView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
