214cd2caf5d532a848e3d6d262c6f3235c0b3e8f
LY <liuyan199108@sina.cn>
2016-05-25 00:50:45 +0800

    练习层级动画

---
+++ ExampleApp/app/src/main/java/com/example/app/animator/LayerDrawableActivity.java
@@ -0,0 +1,15 @@
+package com.example.app.animator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.app.R;
+
+public class LayerDrawableActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layer_drawable);
+    }
+}
